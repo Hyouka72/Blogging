a7546a7b289d0a8168e96014a331d9133693ae91
Hyouka45 <khewang212@gmail.com>
2025-12-02 10:42:35 +0545

    all mapping done of user

--- src/main/java/com/Khewang/blogging/controller/UserController.java
+++ src/main/java/com/Khewang/blogging/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.Khewang.blogging.controller;
 
+import com.Khewang.blogging.payload.ApiResponse;
 import com.Khewang.blogging.payload.UserDto;
 import com.Khewang.blogging.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -15,9 +19,36 @@ public class UserController {
         this.userService = userService;
     }
 
+    //create user
     @PostMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
       UserDto createUserDto =  this.userService.createUser(userDto);
       return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+    //update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto>  updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer id){
+        UserDto updatedUser = this.userService.updateUser(userDto, id);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    //delete user
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer id){
+            this.userService.deleteUser(id);
+            return new ResponseEntity<>(new ApiResponse("User Deleted Sucessfully", true), HttpStatus.OK);
+    }
+
+    //Get user
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getAllUsers(@PathVariable Integer id) {
+        UserDto userDto = this.userService.getUserById(id);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 }
