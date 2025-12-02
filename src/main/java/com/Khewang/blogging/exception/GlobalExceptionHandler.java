2bc03fc8466ecdd0d7e7ec60f66ecce284cedc29
Hyouka45 <khewang212@gmail.com>
2025-12-02 10:52:52 +0545

    custom exception handling

---
+++ src/main/java/com/Khewang/blogging/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.Khewang.blogging.exception;
+
+import com.Khewang.blogging.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFound(ResourceNotFoundException resourceNotFoundException){
+            String message = resourceNotFoundException.getMessage();
+            ApiResponse apiResponse = new ApiResponse(message, false);
+            return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+}
