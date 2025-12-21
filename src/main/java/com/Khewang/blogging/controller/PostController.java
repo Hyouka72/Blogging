b4bcc7d0c1a25b499a70535b3a0a8b8faff14020
Hyouka45 <khewang212@gmail.com>
2025-12-21 17:56:38 +0545

    good practice implemented

--- src/main/java/com/Khewang/blogging/controller/PostController.java
+++ src/main/java/com/Khewang/blogging/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.Khewang.blogging.controller;
 
+import com.Khewang.blogging.config.AppConstants;
 import com.Khewang.blogging.payload.ApiResponse;
 import com.Khewang.blogging.payload.PostDto;
 import com.Khewang.blogging.payload.PostResponse;
@@ -47,10 +48,10 @@ public class PostController {
 
     @GetMapping("/posts")
     public ResponseEntity<PostResponse> getAllPost(
-            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "5", required = false)Integer pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false)Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
     ){
         PostResponse posts = this.postService.getAllPost(pageNumber,pageSize, sortBy , sortDir);
         return new ResponseEntity<>(posts, HttpStatus.OK);
